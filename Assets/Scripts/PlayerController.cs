2105f222312213d06968b2130b13cb81481012c2
Federico Farina <fedefarina02@gmail.com>
2022-06-26 17:48:26 -0300

    animations for character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,16 +9,19 @@ public class PlayerController : MonoBehaviour
     AudioSource audioSource;
     public AudioClip jump;
     public AudioClip death;
+    public Animator animator;
     float speed = 10f;
     float jumpForce =10f;
     float horizontalInput;
     bool isOnGround = true;
+
     
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         capCollider = GetComponent<CapsuleCollider>();
         audioSource = GetComponent<AudioSource>();
+        animator.SetBool("isJumping", false);
     }
 
     // Update is called once per frame
@@ -26,19 +29,22 @@ public class PlayerController : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         rb.AddRelativeForce(Vector3.left * horizontalInput * speed * Time.deltaTime, ForceMode.Impulse);
+        animator.SetBool("isRunning", Input.GetAxis("Horizontal") != 0);
         if(Input.GetKeyDown(KeyCode.W) && isOnGround)
         {
             rb.AddRelativeForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            animator.SetBool("isJumping", true);
+            animator.SetBool("isRunning", false);
             isOnGround = false;
-            audioSource.PlayOneShot(jump, 0.5f);
-        }
-
+            audioSource.PlayOneShot(jump, 0.5f);    
+        }  
     }
 
     private void OnCollisionEnter(Collision collision)
     {
        isOnGround = true;
-       if (collision.gameObject.CompareTag("Enemy"))
+        animator.SetBool("isJumping", false);
+        if (collision.gameObject.CompareTag("Enemy"))
        {
             capCollider.enabled = false;
             rb.AddForce(Vector3.up * 20, ForceMode.Impulse);
